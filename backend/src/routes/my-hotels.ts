d31539ee80ef5d4a7798af3623cf48e225c4f3a2
Rupesh sharma <rsharma@Rk-Air.local>
2024-04-02 21:50:03 +0530

    updated the my-hotels

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -88,13 +88,13 @@ import { body } from "express-validator";
 
 const router = express.Router();
 
-// const storage = multer.memoryStorage();
-// const upload = multer({
-//   storage: storage,
-//   limits: {
-//     fileSize: 5 * 1024 * 1024, // 5MB
-//   },
-// });
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
+});
 
 router.post(
   "/",
@@ -114,7 +114,7 @@ router.post(
       .isArray()
       .withMessage("Facilities are required"),
   ],
-  //upload.array("imageFiles", 6),
+  upload.array("imageFiles", 6),
   async (req: Request, res: Response) => {
 
     // const config = {
@@ -124,14 +124,14 @@ router.post(
     // }
 
     try {
-      // const imageFiles = req.files as Express.Multer.File[];
+      const imageFiles = req.files as Express.Multer.File[];
       const newHotel = req.body;
 
-      // const imageUrls = await uploadImages(imageFiles);
+      const imageUrls = await uploadImages(imageFiles);
 
-      // newHotel.imageUrls = imageUrls;
-      // newHotel.lastUpdated = new Date();
-      // newHotel.userId = req.userId;
+      newHotel.imageUrls = imageUrls;
+      newHotel.lastUpdated = new Date();
+      newHotel.userId = req.userId;
 
       const hotel = new Hotel(newHotel);
       console.log(hotel);
