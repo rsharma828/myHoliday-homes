26551688533c72012f590c418523995f4a30844d
Rupesh sharma <rksharmagmo@gmail.com>
2024-08-13 14:14:27 +0530

    currency change 3

--- frontend/src/forms/GuestInfoForm/GuestFormForm.tsx
+++ frontend/src/forms/GuestInfoForm/GuestFormForm.tsx
@@ -67,7 +67,7 @@ const GuestInfoForm = ({ hotelId, pricePerNight }: Props) => {
 
     return (
         <div className="flex flex-col p-4 bg-blue-200 gap-4">
-            <h3 className="text-md font-bold">£{pricePerNight}</h3>
+            <h3 className="text-md font-bold">Rs {pricePerNight}</h3>
             <form onSubmit={
                 isLoggedIn ? handleSubmit(onSubmit) : handleSubmit(onSignInClick)
             }>
